dec74fa61d7fa4c4521ee65cf884eae9bbdfa803
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-06-25 12:18:54 +0200

    cars

---
+++ src/components/CardComp.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        
+    </div>
+    <div>
+        <h1>
+        <!-- looping through data using slots -->
+         <slot name="make"></slot>
+         <slot name="model"></slot>
+         <slot name="year"></slot>
+         <slot name="color"></slot>
+         <slot name="price"></slot>
+         <slot></slot>
+         </h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
